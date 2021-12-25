6366b25ebc496bf390a60b46257cc5f01fc39ada
Olga Ivanova <olga.a.ivanova@inbox.ru>
2021-12-25 16:39:47 +0300

    say hello

--- src/main/java/plaindoll/Welcomer.java
+++ src/main/java/plaindoll/Welcomer.java
@@ -1,10 +1,16 @@
 package plaindoll;
 
 public class Welcomer{
+
 	public String sayWelcome() {
 		return "Welcome home, good hunter. What is it your desire?";
 	}
+
 	public String sayFarewell() {
 		return "Farewell, good hunter. May you find your worth in waking world.";
+	}
+
+	public String sayHello() {
+		return "Hello, I'm hunter";
 	}
 }
